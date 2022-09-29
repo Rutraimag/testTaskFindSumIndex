b9adea097e8054926bcf2023e385e66be3bc654c
Rutraimag <gamidov.artur2000@mail.ru>
2022-09-29 14:01:08 +0300

    fixed wrong algo. not find sum, but find where the remainder is zero

--- FindSum/FindSum/MainClass.cs
+++ FindSum/FindSum/MainClass.cs
@@ -15,6 +15,11 @@ namespace FindSum
             start = 0;
             end = 0;
             List<ulong> remainder = new List<ulong>(list.Count);
+            if (list[0] == sum)
+            {
+                end = 1;
+                return;
+            }
             remainder.Add(list[0] % sum);
             HashSet<ulong> set = new HashSet<ulong>();
             if (list[0] != 0)
@@ -25,6 +30,11 @@ namespace FindSum
             for (int i = 1; i < list.Count() && ost == null; ++i)
             {
                 remainder.Add(list[i] == 0 ? sum : (remainder[i - 1] +  list[i] % sum) % sum);
+                if (remainder[i] == 0)
+                {
+                    end = i + 1;
+                    return;
+                }
                 if (set.Contains(remainder[i]))
                 {
                     ost = remainder[i];
@@ -58,9 +68,22 @@ namespace FindSum
             int start, end;
             FindElementsForSum(new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 }, 11, out start, out end); //start будет равен 5 и end 7;
             System.Console.WriteLine(start + " " + end);
+            FindElementsForSum(new List<uint> { 0, 1, 2, 3, 4, 5, 0, 0, 0, 6, 7 }, 11, out start, out end); //start будет равен 5 и end 10;
+            System.Console.WriteLine(start + " " + end);
             FindElementsForSum(new List<uint> { 4, 5, 6, 7 }, 18, out start, out end); //start будет равен 1 и end 4;
             System.Console.WriteLine(start + " " + end);
+            FindElementsForSum(new List<uint> { 4, 5, 6, 7 }, 4, out start, out end); //start будет равен 0 и end 1;
+            System.Console.WriteLine(start + " " + end);
             FindElementsForSum(new List<uint> { 0, 1, 2, 3, 4, 5, 6, 7 }, 88, out start, out end); //start будет равен 0 и end 0;
+            System.Console.WriteLine(start + " " + end);
+
+            var testList = new List<uint>(5000000);
+
+            for(uint i = 0; i < 5000000; ++i)
+            {
+                testList.Add(1);
+            }
+            FindElementsForSum(testList, 4999999, out start, out end); //start будет равен 0 и end 4999999;
             System.Console.WriteLine(start + " " + end);
         }
     }
